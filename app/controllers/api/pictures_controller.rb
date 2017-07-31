05634c6c504e29563eafc249ed7dd99df75b47aa
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-07-31 20:13:29 +0900

    Sort pictures by name

--- app/controllers/api/pictures_controller.rb
+++ app/controllers/api/pictures_controller.rb
@@ -2,6 +2,7 @@ class Api::PicturesController < ApplicationController
   def index
     pictures = Dir.glob("public/images/slick/*")
     pictures = pictures.map { |p| p.gsub(/\Apublic/, "") }
+                       .sort { |a, b| a <=> b } 
 
     render json: pictures
   end
